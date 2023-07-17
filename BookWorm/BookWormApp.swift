a008dff975c37670bf9536b0d320a7ffe571259a
Kitwana Akil <kakil@me.com>
2023-07-17 08:32:31 -0400

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Kitwana Akil on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
